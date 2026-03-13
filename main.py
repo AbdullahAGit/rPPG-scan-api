6deb4e1a0db47983aa586ff1a78a6888b10b81da
AbdullahA <46983048+AbdullahAGit@users.noreply.github.com>
2026-03-12 22:02:51 -0400

    updated api for model.py

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import torch
 import numpy as np
+
+from model import PPGtoBPNet
 
 app = FastAPI()
 
+device = torch.device("cpu")
+
+# load model once when server starts
+model = PPGtoBPNet()
+model.load_state_dict(torch.load("ppg_to_bp.pth", map_location=device))
+model.eval()
+
+
+class SignalInput(BaseModel):
+    signal: list[float]
+
+
 @app.get("/")
-def home():
-    return {"message": "API is running"}
+def root():
+    return {"message": "API running"}
+
 
 @app.post("/predict")
-def predict(data: dict):
+def predict(data: SignalInput):
 
-    signal = np.array(data["signal"])
+    signal = np.array(data.signal)
 
-    # dummy prediction for now
-    test1 = float(signal.mean() * 100)
-    test2 = float(signal.mean() * 60)
+    # normalize like training
+    signal = (signal - signal.mean()) / (signal.std()+1e-6)
+
+    tensor = torch.tensor(signal).float().unsqueeze(0).unsqueeze(0)
+
+    with torch.no_grad():
+        pred = model(tensor).numpy()[0]
+
+    systolic = float(pred[0])
+    diastolic = float(pred[1])
 
     return {
-        "systolic": test1,
-        "diastolic": test2
+        "systolic": systolic,
+        "diastolic": diastolic
     }
